ba93e52f6b982b1e9c268f14940bd050ba6870ea
Jurij Srebrnič <jurij.srebrnic@gmail.com>
2022-12-11 17:01:11 +0100

    Solved day 9 part 2

--- day-9/main.py
+++ day-9/main.py
@@ -1,18 +1,48 @@
 import argparse
+import copy
 
-def is_touching(head, tail):
+class TailNode():
+    def __init__(self, start_y, start_x, pos):
+        self.y = start_y
+        self.x = start_x
+        self.position = pos
+        self.next = None
+
+    def __repr__(self):
+        return f'pos: {self.position} y: {self.y}, x: {self.x}'
+
+def are_nodes_touching(node_1, node_2):
     # Get the absolute difference between the head and tail y and x positions
     # Especially in the tricky cases where head = (-2, -1) and tail (-1, -1).
     # The head and tail are touching when the y and x diff are < 2
 
-    diff_y = abs(head[0] - tail[0])
-    diff_x = abs(head[1] - tail[1])
+    diff_y = abs(node_1.y - node_2.y)
+    diff_x = abs(node_1.x - node_2.x)
     
-    print(f"diff y {diff_y} diff x {diff_x}")
-
     if diff_y < 2 and diff_x < 2:
         return True
     return False
+
+def get_nodes_diff(node_1, node_2):
+    # Get the diff in y and x position between nodes, in other words
+    # get the direction in which node 2 need to move to arrive at the 
+    # correct position. Before returning we need to normalize the result, 
+    # we don't want values larger that 1
+
+    diff_y = node_1.y - node_2.y
+    diff_x = node_1.x - node_2.x
+
+    if diff_x > 1:
+        diff_x = 1
+    if diff_x < -1:
+        diff_x = -1
+
+    if diff_y > 1:
+        diff_y = 1
+    if diff_y < -1:
+        diff_y = -1
+
+    return diff_y, diff_x
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -22,48 +52,67 @@ if __name__ == "__main__":
     with open(args.input) as f:
         lines = f.readlines()
 
-    tail_index = 0
+    # Create starting position for tail and 
+    # initialize all the tail nodes
+    tail_start = TailNode(0, 0, 0)
     
-    positions = [(0, 0)]
-    unique_positions = {}
+    current = None
+    for i in range(0, 9):
+        if not current:
+            current = tail_start
 
-    key = f"{positions[0][0]}|{positions[0][1]}"
-    unique_positions[key] = 0
+        current.next = TailNode(0, 0, i+1)
+        current = current.next
+
+    # initialize unique positions, we are using 
+    # the formant y|x for storing the keys
+    unique_positions_part_1 = {"0|0": 0}
+    unique_positions_part_2 = {"0|0": 0}
+
+    # Staring position for the rope head
+    y, x = 0, 0
 
     for line in lines:
         line = line.strip()
 
         direction, steps = line.split(" ")[0], int(line.split(" ")[1])
-        print(f"Direction {direction}, steps {steps}")
-
+        
         for s in range(0, steps):
-            new_x = positions[-1][1]
-            new_y = positions[-1][0]
-
             if direction == "R":
-                new_x += 1
+                x += 1
             if direction == "L":
-                new_x -= 1
+                x -= 1
             if direction == "U":
-                new_y += 1
+                y += 1
             if direction == "D":
-                new_y -= 1
+                y -= 1
 
-            positions.append((new_y, new_x))
-            print(f"New position position {positions[-1]}, tail position {positions[tail_index]}")
+            next_node = TailNode(y, x, -1)
 
-            if len(positions) < 2:
-                continue
+            current = tail_start
 
-            if not is_touching(positions[-1], positions[tail_index]):
-                tail_index = len(positions) -2
-                print(f"Not touching, catching up new {positions[tail_index]}")
+            while current.next:
+                if are_nodes_touching(next_node, current):
+                    break
+                
+                tmp = current
 
-                key = f"{positions[tail_index][0]}|{positions[tail_index][1]}"
-                if not unique_positions.get(key):
-                    unique_positions[key] = 0
- 
-        print("-------------------------------------------\n")
+                diff_y, diff_x = get_nodes_diff(next_node, current)
 
-    print(unique_positions)
-    print(f"Unique {len(unique_positions)}")
+                current.y = current.y + 1 * diff_y
+                current.x = current.x + 1 * diff_x
+
+                if current.position == 0:
+                    key = f"{current.y}|{current.x}"
+                    if not unique_positions_part_1.get(key):
+                        unique_positions_part_1[key] = 0
+
+                if current.position == 8:
+                    key = f"{current.y}|{current.x}"
+                    if not unique_positions_part_2.get(key):
+                        unique_positions_part_2[key] = 0
+
+                next_node = tmp
+                current = current.next
+
+    print(f"Part 1: {len(unique_positions_part_1)}, part 2: {len(unique_positions_part_2)}")
